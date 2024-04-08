a2b91df7b0e7ab5e720f3e0697a7e91df2c4e859
NestorLiao <gtkndcbfhr@gmail.com>
2024-04-08 18:07:48 +0800

    rust update and python pip

--- go/flake.nix
+++ go/flake.nix
@@ -1,31 +1,50 @@
 {
   description = "A Nix-flake-based Go 1.17 development environment";
 
+  nixConfig = {
+    builders-use-substitutes = true;
+    experimental-features = ["nix-command" "flakes"];
+    substituters = [
+      # "https://mirror.sjtu.edu.cn/nix-channels/store"
+      "https://mirrors.ustc.edu.cn/nix-channels/store"
+      "https://cache.nixos.org/"
+    ];
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
 
-  outputs = { self, nixpkgs }:
-    let
-      goVersion = 20; # Change this to update the whole stack
-      overlays = [ (final: prev: { go = prev."go_1_${toString goVersion}"; }) ];
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit overlays system; };
-      });
-    in
-    {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            # go 1.20 (specified by overlay)
-            go
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    goVersion = 20; # Change this to update the whole stack
+    overlays = [(final: prev: {go = prev."go_1_${toString goVersion}";})];
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import nixpkgs {inherit overlays system;};
+        });
+  in {
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          # go 1.20 (specified by overlay)
+          go
 
-            # goimports, godoc, etc.
-            gotools
+          # goimports, godoc, etc.
+          gotools
 
-            # https://github.com/golangci/golangci-lint
-            golangci-lint
-          ];
-        };
-      });
-    };
+          # https://github.com/golangci/golangci-lint
+          golangci-lint
+        ];
+      };
+    });
+  };
 }
