1281e23db5fddc5404d3a4f7fd1c4399d4c7bc9f
NestorLiao <gtkndcbfhr@gmail.com>
2024-04-24 09:03:01 +0800

    pythonedit

--- python/flake.nix
+++ python/flake.nix
@@ -3,6 +3,23 @@
 
   inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
 
+  nixConfig = {
+    builders-use-substitutes = true;
+    experimental-features = ["nix-command" "flakes"];
+    substituters = [
+      "https://mirror.sjtu.edu.cn/nix-channels/store"
+      "https://mirrors.ustc.edu.cn/nix-channels/store"
+      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+      "https://cache.nixos.org/"
+    ];
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   outputs = {
     self,
     nixpkgs,
@@ -13,8 +30,8 @@
         f {
           pkgs = import nixpkgs {
             inherit system;
-            config.allowUnfree = true;
-            cudaSupport = true;
+            # config.allowUnfree = true;
+            # cudaSupport = true;
           };
         });
   in {
@@ -29,8 +46,11 @@
           (with python3Packages; [
             python
             venvShellHook
-            numpy
-            request
+            # numpy
+            # pandas
+            # matplotlib
+            # scipy
+            # requests
           ])
 
           taglib
@@ -45,15 +65,16 @@
         # Run this command, only after creating the virtual environment
         postVenvCreation = ''
           unset SOURCE_DATE_EPOCH
-          pip install -r requirements.txt
+          # pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+          # pip install -r requirements.txt
         '';
 
         # Now we can execute any commands within the virtual environment.
         # This is optional and can be left out to run pip manually.
         postShellHook = ''
           # allow pip to install wheels
-          export QT_QPA_PLATFORM_PLUGIN_PATH="${pkgs.qt5.qtbase.bin}/lib/qt-${pkgs.qt5.qtbase.version}/plugins/platforms";
-          export PYTHON_CONFIGURE_OPTS="--enable-shared"
+          # export QT_QPA_PLATFORM_PLUGIN_PATH="${pkgs.qt5.qtbase.bin}/lib/qt-${pkgs.qt5.qtbase.version}/plugins/platforms";
+          # export PYTHON_CONFIGURE_OPTS="--enable-shared"
           unset SOURCE_DATE_EPOCH
         '';
       };
